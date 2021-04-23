2bcf8f76109615fcd4fa0e62d03dd9c33b5a335f
sieuwe1 <sieuwe_crossen@hotmail.com>
2021-04-23 17:19:08 +0200

    finisdhed program + first dataset

--- board.py
+++ board.py
@@ -2,22 +2,22 @@ import argparse
 import time
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
 board = None 
+data_dir = None
 
 def connect_to_headset(serial_port):
     global board
     params = BrainFlowInputParams()
-    #params.serial_port = serial_port
-    #board = BoardShim(2, params) #cython board has id 0. with daisy id 2
-    board = BoardShim(BoardIds.SYNTHETIC_BOARD.value, params)
+    params.serial_port = serial_port
+    board = BoardShim(BoardIds.CYTON_DAISY_BOARD, params) #cython board has id 0. with daisy id 2
+    #board = BoardShim(BoardIds.SYNTHETIC_BOARD.value, params) fake board
     board.prepare_session()
-    board.start_stream()
-    #time.sleep(10)
     return "connected"
 
 def close_headset():
@@ -28,45 +28,30 @@ def close_headset():
 def write_to_csv():
     return
 
-def get_data():
-    data = board.get_board_data()  # get 20 latest data points dont remove them from internal buffer
-    print(data.shape)
+def start_data_stream():
+    board.start_stream()
+
+def stop_data_stream():
+    data = board.get_current_board_data(250)
+    board.stop_stream()
+
     sample = []
-    eeg_channels = BoardShim.get_eeg_channels(BoardIds.SYNTHETIC_BOARD.value)
+    eeg_channels = BoardShim.get_eeg_channels(BoardIds.CYTON_DAISY_BOARD.value)
+    #print("channels = " + str(eeg_channels))
     for channel in eeg_channels:
         sample.append(data[channel])
+    return sample
 
-    print(np.array(sample).shape)
-    for j in range(7, 8):
-        plt.plot(np.arange(len(sample[j])), sample[j])
-    plt.show()
+def create_directory(directoy_name):
+    global data_dir
+    if not os.path.exists(directoy_name):
+        os.mkdir(directoy_name)
+    data_dir = directoy_name
 
-    #save_sample(np.array(sample), ACTIONS[last_act])
-    return None
+def save_sample(sample, action):
+    actiondir = f"{data_dir}/{action}"
+    if not os.path.exists(actiondir):
+        os.mkdir(actiondir)
 
-
-def main():
-    board.prepare_session()
-    board.start_stream()
-    BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
-    time.sleep(10)
-    data = board.get_current_board_data() # get 20 latest data points dont remove them from internal buffer
-    board.stop_stream()
-    board.release_session()
-
-    # demo how to convert it to pandas DF and plot data
-    eeg_channels = BoardShim.get_eeg_channels(BoardIds.SYNTHETIC_BOARD.value)
-    df = pd.DataFrame(np.transpose(data))
-    print('Data From the Board')
-    print(df.head(10))
-
-    # demo for data serialization using brainflow API, we recommend to use it instead pandas.to_csv()
-    DataFilter.write_file(data, 'test.csv', 'w')  # use 'a' for append mode
-    restored_data = DataFilter.read_file('test.csv')
-    restored_df = pd.DataFrame(np.transpose(restored_data))
-    print('Data From the File')
-    print(restored_df.head(10))
-
-
-if __name__ == "__main__":
-    main()
+    print(f"saving {action} personal_dataset...")
+    np.save(os.path.join(actiondir, f"{int(time.time())}.npy"), np.array(sample))
--- training_gui.py
+++ training_gui.py
@@ -1,63 +1,99 @@
 import board as eeg_headset
 import cv2
 import time
+import numpy as np
+import random
 
-sample_count = 65
+sample_count = 60
 gui_name = "data trainer"
+dir_name = "data_set_forward_yaw_right_empty"
+com_port = "/dev/ttyUSB0"
+
+current_sample_count = [0,0,0,0,0,0,0,0,0]
+#directions = ["forward","backward","up","down","yaw left", "yaw right", "left", "right", "empty"]
+directions = ["forward", "yaw right", "empty"]
+
+def generate_random():
+    return random.randint(0,len(directions)-1)
+
+def generate_random_list():
+    dir_list = []
+
+    for i in range(sample_count * len(directions)):
+        print("i = ", i)
+
+        random = generate_random()
+
+        while dir_list.count(random) >= sample_count:
+            random = generate_random()
+
+        dir_list.append(random) 
+
+    return dir_list
+
+directions_list = generate_random_list()
 
 def sample_data(direction):
-    
+
+    frame_count = 0
+    recording = False
     cap = None
-    current_sample_count = 0
+    sample = None
+    start_time = 30
 
     if direction == "forward":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_forward.mp4')
     elif direction == "backward":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_back.mp4')
 
     elif direction == "up":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_up.mp4')
     elif direction == "down":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_down.mp4')
 
     elif direction == "yaw left":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_turn_left.mp4')
     elif direction == "yaw right":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_turn_right.mp4')
 
     elif direction == "left":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_left.mp4')
     elif direction == "right":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_right.mp4')
 
     elif direction == "empty":
-        cap = cv2.VideoCapture('media/forward.mkv')
+        cap = cv2.VideoCapture('media/drone_hover.mp4')
 
     while(cap.isOpened()):
         ret, frame = cap.read()
         time.sleep(1/50)
-        print(eeg_headset.get_data())
+        
+        if frame_count == start_time:
+            eeg_headset.start_data_stream()
+            recording = True
+
+        elif frame_count == start_time + 65:
+            sample = eeg_headset.stop_data_stream()
+            recording = False
+
         if ret:
-            if current_sample_count == 0:
-                cv2.putText(frame, "Think about the drone moving: "  + str(direction), (50,500), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 255), 3, cv2.LINE_AA) 
-            else:
-                cv2.putText(frame, str(current_sample_count) + " samples complete from " + str(sample_count) + " samples", (50,50), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 255), 3, cv2.LINE_AA) 
+            cv2.putText(frame, str(current_sample_count) + " samples complete from " + str(sample_count) + " samples", (50,50), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 255), 3, cv2.LINE_AA) 
+            cv2.putText(frame, "Think about the drone moving: "  + str(direction), (50,500), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 255), 3, cv2.LINE_AA) 
+
+            if recording:
+                cv2.putText(frame, "recording now!", (50,100), cv2.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 255), 3, cv2.LINE_AA) 
+
             cv2.imshow(gui_name,frame)
 
         else:
-            print('vid ended')
-            cap.set(cv2.CAP_PROP_POS_FRAMES, -125)
-
-            if current_sample_count >= sample_count:
-                cap.release()
-
-            else:
-               current_sample_count+=1
+            cap.release()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+        frame_count += 1
 
+    eeg_headset.save_sample(sample,direction)
 
 home_image = cv2.imread("media/home.png")
 image_width, image_height, channels = home_image.shape
@@ -75,6 +111,11 @@ cv2.putText(home_image, "if you are ready then lets begin by pressing any key!",
 cv2.imshow(gui_name, home_image)
 cv2.waitKey(0) 
 
-eeg_headset.connect_to_headset("/dev/ttyUSB0")
+eeg_headset.connect_to_headset(com_port)
+eeg_headset.create_directory(dir_name)
 
-sample_data("forward")
+for i in directions_list:
+
+    print("direction = " + str(directions[i]))
+    current_sample_count[i] += 1
+    sample_data(directions[i])
